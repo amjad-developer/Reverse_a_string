f458d2581c54b9d852c3d57058e4206c0a9e4dd0
Amjad <40330941+amjad-developer@users.noreply.github.com>
2018-11-30 20:54:33 +0000

    Update String_Reverse.py

--- String_Reverse.py
+++ String_Reverse.py
@@ -1,4 +1,2 @@
-def reverse(string):
-    return ''.join(reversed(string))
 string = input("Enter a string to Reverse: ")
-print("Reversed string: " + reverse(string))
+print("Reversed string: " + ''.join(reversed(string)))
